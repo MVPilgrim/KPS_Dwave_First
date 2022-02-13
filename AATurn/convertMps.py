03810dfb0c64c9a84ee189e4be655b3cb01591bd
MVPilgrim <mv.pilgrim.aws@gmail.com>
2022-02-13 21:43:39 +0000

    Saving

--- AATurn/convertMps.py
+++ AATurn/convertMps.py
@@ -49,11 +49,6 @@ def parse_mps(data_file):
             elif line == "ENDATA":
                 callBounds = False
                 processEndata()
-                print("End of file.")
-                print("obj func name: ",objFuncName)
-                print("objFuncVarNamesAndCoeffs: ",objFuncVarNamesAndCoeffs)
-                print("allConstraintNamesAndLists: ",allConstraintNamesAndLists)
-                print("rhsConstraintsAndValues: ",rhsConstraintsAndValues)
             elif callRows:
                 processRows(line)
             elif callColumns:
@@ -67,36 +62,28 @@ def parse_mps(data_file):
 def addConstraintVal(constraintName,varName,val):
     # Map: constraint name and corresponding array: [constraint type,[[var name,value]]]
     ws = allConstraintNamesAndLists.get(constraintName,"xxx")
-    print("addConstraintVal(): ws: ",ws)
     if ws != "xxx":
         varvalList = ws + [[varName,val]]
-        print("varvalLIst: ",varvalList)
         allConstraintNamesAndLists[constraintName] = varvalList
-        print("allConstraintNamesAndLists: ",allConstraintNamesAndLists)
     else:
         print("addConstraintVal(): constraintName not in all constraints: ",constraintName)
 
 def processRows(line):
-    print("processRows line: ",line)
     wl = line.split()
 
     if wl[0] == 'N':
         objFuncName = wl[1]
-        print("processRows() objFuncName: ",objFuncName)
+        print("objFuncName,wl[1]: ",objFuncName,",",wl[1])
     else:
         #allConstraintNamesAndLists = {wl[1]:[]}
         allConstraintNamesAndLists[wl[1]] = [wl[0]]
-        print("allConstraintNamesAndLists: ",allConstraintNamesAndLists)
 
 def processColumns(line):
-    print("processColumns() line: ",line)
     wl = line.split()
-    print("processColumns() array: ",wl)
 
     if wl[1] == 'COST':
         # wl: dvarName[0],"COST"[1],val[2][,Constraint name[3], constraint val[4]]
         objFuncVarNamesAndCoeffs[wl[0]] = wl[2]
-        print("ProcessColumns(): wl.count(): ",len(wl))
         if len(wl) == 5: 
             try:
                 addConstraintVal(wl[3],wl[0],wl[4])
@@ -108,9 +95,7 @@ def processColumns(line):
         print("Invalid COLUMN type: ", wl)
 
 def processRhs(line):
-    print("processRhs line: ",line)
     wl = line.split()
-    print("processColumns() array: ",wl)
 
     rhsConstraintsAndValues[wl[1]] = wl[2]
 
@@ -122,22 +107,26 @@ def processRhs(line):
 
 
 def processBounds(line):
-    print("processBounds line: ",line)
     wl = line.split()
-    print("processBounds() array: ",wl)
 
     # [constraint type,[[var name,value]]]
     if wl[0] == 'UP':
-        upVars = list(upBoundsandValues[wl[1]])
-        upBoundsAndValues[wl[1]] = upVars + [wl[2],wl[3]]
+        if upBoundsAndValues.__len__() <= 0:
+            upVars = ["UP"]
+        else:
+            upVars = list(upBoundsAndValues[wl[1]])
+        upBoundsAndValues[wl[1]] = upVars + [[wl[2],wl[3]]]
     elif wl[0] == 'LO':
-        loVars = list(upBoundsandValues[wl[1]])
-        loBoundsAndValues[wl[1]] = loVars + [wl[2],wl[3]]
+        if loBoundsAndValues.__len__() <= 0:
+            loVars = ["LO"]
+        else:
+            loVars = list(upBoundsAndValues[wl[1]])
+        loBoundsAndValues[wl[1]] = loVars + [[wl[2],wl[3]]]
     else:
         print("Invalid BOUNDS type: ", wl)
     
 def processEndata():
-    print("End of file.")
+    print("\nEnd of file.")
     print("obj func name: ",objFuncName)
     print("objFuncVarNamesAndCoeffs: ",objFuncVarNamesAndCoeffs)
     print("allConstraintNamesAndLists: ",allConstraintNamesAndLists)
